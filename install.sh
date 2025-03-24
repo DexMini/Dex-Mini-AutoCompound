367d7b613867b942529ec0418671623a991cf838
JosueMpia <smithlufoko@rocketmail.com>
2025-03-23 20:20:09 -0400

    git commit -m "Update AutoCompoundHook test implementation"

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-forge install --no-commit OpenZeppelin/openzeppelin-contracts
-forge install --no-commit Uniswap/v4-core
-forge install --no-commit Uniswap/v4-periphery
-
-# Try to build
-forge build 
